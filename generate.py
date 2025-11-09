ed38fd8e719753f18a6088e4a464c3042bef7320
Jannik Vogel <email@jannikvogel.de>
2025-11-09 22:27:26 +0100

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+statePath = sys.argv[1]
+pagesPath = sys.argv[2]
+
+state = json.loads(open(statePath + "/state.json", 'rb').read())
+
+indexHtml = "<b>Hello world %d</b>" % (state['value'])
+
+open(pagesPath + "/index.html", 'wb').write(indexHtml.encode('utf-8'))
+
+
